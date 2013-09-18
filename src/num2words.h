62c4baa571451bd60b2cc2452206edf09caf41a9
Marckus <marckdavis1@gmail.com>
2013-09-18 13:17:23 -0400

    Update num2words.h

--- src/num2words.h
+++ src/num2words.h
@@ -3,15 +3,8 @@
 #include "pebble_os.h"
 #include "pebble_app.h"
 #include "string.h"
-
-#define DAY 0
 #define BUFFER_SIZE 43
-
-#if DAY
-#define DATE_FORMAT "%a %B %e"
-#else
-#define DATE_FORMAT "%B %e"
-#endif
+#define DATE_FORMAT "%a, %b %e"  //added Day of Week "%a," - MSD 9-18-13
 
 void fuzzy_minutes_to_words(PblTm *t, char* words);
 void fuzzy_hours_to_words(PblTm *t, char* words);
